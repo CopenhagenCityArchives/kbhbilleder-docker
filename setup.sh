5d5c3a3d7c42f0083123f854088fc333afa48751
Mathias Larsen <mathias@reload.dk>
2018-04-10 16:00:52 +0200

    Fix linking again

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 touch ./kbh-billeder/google-key.json
 npm-link-shared collections-online kbh-billeder && npm-link-shared collections-online-cumulus kbh-billeder
-cd ./kbh-billeder && npm install
+cd ./kbh-billeder && npm install && npm link ../collections-online && npm link ../collections-online-cumulus
 ../wait-for-it.sh elasticsearch:9200 -- npm run start:dev
